dcf2b736c14714b426623c5d45a74c350e109aaa
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-09-06 20:49:47 -0400

    v1.0.1

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,19 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '.',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-  }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: new URL('./index.html', import.meta.url).pathname,
+        login: new URL('./login.html', import.meta.url).pathname,
+        chat: new URL('./chat.html', import.meta.url).pathname,
+      }
+    }
+  },
 })
